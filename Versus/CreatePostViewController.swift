e22169f43f35907d0c6ffac24773ba2315654889
dlee121 <dlee121@illinois.edu>
2018-08-15 18:34:19 -0400

    CreatePost UI setup

---
+++ Versus/CreatePostViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreatePostViewController.swift
+//  Versus
+//
+//  Created by Dongkeun Lee on 8/15/18.
+//  Copyright © 2018 Versus. All rights reserved.
+//
+
+import UIKit
+
+class CreatePostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+}
