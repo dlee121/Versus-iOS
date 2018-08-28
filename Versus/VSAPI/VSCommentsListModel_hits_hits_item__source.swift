4a3f2cf88211af96e6a7d1c747a808bca3be3379
dlee121 <dlee121@illinois.edu>
2018-08-28 00:47:49 -0400

    api sdk update

--- Versus/VSAPI/VSCommentsListModel_hits_hits_item__source.swift
+++ Versus/VSAPI/VSCommentsListModel_hits_hits_item__source.swift
@@ -1,18 +1,18 @@
 /*
  Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
+ 
  Licensed under the Apache License, Version 2.0 (the "License").
  You may not use this file except in compliance with the License.
  A copy of the License is located at
-
+ 
  http://aws.amazon.com/apache2.0
-
+ 
  or in the "license" file accompanying this file. This file is distributed
  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  express or implied. See the License for the specific language governing
  permissions and limitations under the License.
  */
- 
+
 
 import Foundation
 import AWSCore
@@ -29,21 +29,23 @@ public class VSCommentsListModel_hits_hits_item__source : AWSModel {
     var pt: String?
     var t: String?
     var m: NSNumber?
+    var rc: NSNumber?
     var ct: String?
     
-   	public override static func jsonKeyPathsByPropertyKey() -> [AnyHashable : Any]!{
-		var params:[AnyHashable : Any] = [:]
-		params["a"] = "a"
-		params["pr"] = "pr"
-		params["ci"] = "ci"
-		params["r"] = "r"
-		params["u"] = "u"
-		params["d"] = "d"
-		params["pt"] = "pt"
-		params["t"] = "t"
-		params["m"] = "m"
-		params["ct"] = "ct"
-		
+    public override static func jsonKeyPathsByPropertyKey() -> [AnyHashable : Any]!{
+        var params:[AnyHashable : Any] = [:]
+        params["a"] = "a"
+        params["pr"] = "pr"
+        params["ci"] = "ci"
+        params["r"] = "r"
+        params["u"] = "u"
+        params["d"] = "d"
+        params["pt"] = "pt"
+        params["t"] = "t"
+        params["m"] = "m"
+        params["rc"] = "rc"
+        params["ct"] = "ct"
+        
         return params
-	}
+    }
 }
